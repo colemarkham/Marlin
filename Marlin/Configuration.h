1f86ff3f34b0b35c70788d77b0a166acfe22211b
Cole Markham <cole@ccmcomputing.net>
2020-09-09 12:59:39 -0500

    Change max travel for pipette

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -895,7 +895,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 75
-#define Y_BED_SIZE 35
+#define Y_BED_SIZE 55
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
